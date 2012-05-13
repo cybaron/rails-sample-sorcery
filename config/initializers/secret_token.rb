84556465088724564df0697b64641ad2c8c97cb6
Kazuki Asano <cybaron@gmail.com>
2012-05-13 13:59:58 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleSorcery::Application.config.secret_token = '84161e13248c467021b51f9e10a8bf3a907b2e460eddaf166e350422d269ab9a1988dfe5c7a8ced5826f032b84f02e7366fdeefed5366323094568c28910ebbe'
